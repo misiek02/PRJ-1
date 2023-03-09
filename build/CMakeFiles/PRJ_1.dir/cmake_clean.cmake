5881bffc232250b38f984f71ffad6bfd0e489006
paliwko <263519@student.pwr.edu.pl>
2023-03-09 21:39:45 +0100

    dodanie sfml do rysowanka

---
+++ build/CMakeFiles/PRJ_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PRJ_1.dir/src/main.cpp.o"
+  "CMakeFiles/PRJ_1.dir/src/main.cpp.o.d"
+  "PRJ_1"
+  "PRJ_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PRJ_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
